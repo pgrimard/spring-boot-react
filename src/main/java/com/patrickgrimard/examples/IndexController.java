db52d068ca978d66109cb2dd8b6a59a87578f23f
Patrick Grimard <pgrimard@gmail.com>
2016-11-23 15:39:40 -0500

    Index mapping now sends back the request url as the location state object.

--- src/main/java/com/patrickgrimard/examples/IndexController.java
+++ src/main/java/com/patrickgrimard/examples/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +18,13 @@ import java.util.Map;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{path:(?!.*.js|.*.css|.*.jpg).*$}")
-    public String index() {
+    @GetMapping("/{path:(?!.*.js|.*.css|.*.jpg).*$}/**")
+    public String index(Model model, HttpServletRequest request) {
+        Map<String, Object> state = new HashMap<>();
+
+        state.put("location", request.getServletPath() + "?" + request.getQueryString());
+
+        model.addAttribute("state", state);
         return "index";
     }
 
