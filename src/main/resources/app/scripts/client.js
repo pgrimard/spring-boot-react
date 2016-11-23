3cfed6a58a816a4d12fe4c2b069358cd6c0ccf2c
Patrick Grimard <pgrimard@gmail.com>
2016-11-23 15:37:54 -0500

    client.js now renters routes

--- src/main/resources/app/scripts/client.js
+++ src/main/resources/app/scripts/client.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import App from 'components/App';
+import routes from './routes';
 
-render(<App/>, document.getElementById('app'));
+render(routes, document.getElementById('app'));
