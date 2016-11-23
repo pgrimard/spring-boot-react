aa5a46cca9b06f36a4b2d0cd255aee6b7b941308
Patrick Grimard <pgrimard@gmail.com>
2016-11-23 15:37:20 -0500

    App now simply a container

--- src/main/resources/app/scripts/components/App.js
+++ src/main/resources/app/scripts/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'styles/main.css';
 
-export default function App() {
-  return <h1>Hello React!</h1>;
+export default function App({children}) {
+  return <div>{children}</div>;
 }
