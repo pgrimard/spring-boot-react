68673111d91bedb64f1a8bfda2ab6c0bba279051
Patrick Grimard <pgrimard@gmail.com>
2016-11-22 22:52:45 -0500

    Added main.js

---
+++ src/main/resources/app/scripts/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+
+function App(props) {
+  return <ul>{Object.keys(props).map((key) => <li key={key}>{props[key]}</li>)}</ul>;
+}
+
+window.render = (template, {state}) => {
+  if(!state)
+    return template;
+
+  return template.replace('{{output}}', renderToString(<App {...JSON.parse(state)}/>));
+};
