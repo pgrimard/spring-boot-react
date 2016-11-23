10c2ede39bdff8866a4de7130af59352795391d6
Patrick Grimard <pgrimard@gmail.com>
2016-11-23 13:21:38 -0500

    server.js no longer passes server props to App

--- src/main/resources/app/scripts/server.js
+++ src/main/resources/app/scripts/server.js
@@ -2,4 +2,4 @@ import React from 'react';
 import {renderToString} from 'react-dom/server';
 import App from 'components/App';
 
-window.render = (template, {state}) => template.replace('{{output}}', renderToString(<App {...JSON.parse(state)}/>));
+window.render = (template) => template.replace('{{output}}', renderToString(<App/>));
