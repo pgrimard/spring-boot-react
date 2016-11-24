cb0a993eeff71f4b9aad132bffa1f06ef523a762
Patrick Grimard <pgrimard@gmail.com>
2016-11-24 14:06:52 -0500

    /items endpoint now produces JSON

--- src/main/java/com/patrickgrimard/examples/IndexController.java
+++ src/main/java/com/patrickgrimard/examples/IndexController.java
@@ -2,9 +2,11 @@ package com.patrickgrimard.examples;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -34,17 +36,14 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/items")
-    public String hello(Model model) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-
+    @GetMapping(value = "/items", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public Map<String, Object> hello() {
         Map<String, Object> items = new HashMap<>();
         items.put("0", "javascript");
         items.put("1", "react");
         items.put("2", "react-router");
         items.put("3", "redux");
-
-        model.addAttribute("state", mapper.writeValueAsString(items));
-        return "index";
+        return items;
     }
 }
