838dfc62b3afd550c8f77545692065739e143023
Patrick Grimard <pgrimard@gmail.com>
2016-11-24 13:12:56 -0500

    Main component now uses React Router 4 components for routing.

--- src/main/resources/app/scripts/components/Main.js
+++ src/main/resources/app/scripts/components/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Match, Link, Miss} from 'react-router';
+import Home from 'components/Home';
+import Child from 'components/Child';
+import NotFound from 'components/NotFound';
+import 'styles/main.css';
 
 export default function Main() {
   return (
     <div>
       <h1>Hello Server Side Rendering!!</h1>
+
+      <Link to="/">Home</Link>
+      <Link to="/child">Child</Link>
+
+      <Match exactly pattern="/" component={Home}/>
+      <Match pattern="/child" component={Child}/>
+
+      <Miss component={NotFound}/>
     </div>
   );
 }
