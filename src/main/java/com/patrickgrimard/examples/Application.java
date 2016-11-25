ed4582c0fb4427170886bf1d54775a35309499de
Patrick Grimard <pgrimard@gmail.com>
2016-11-25 16:06:03 -0500

    Added initial data to h2 database and exposed ids for Item entities through rest repositories.

--- src/main/java/com/patrickgrimard/examples/Application.java
+++ src/main/java/com/patrickgrimard/examples/Application.java
@@ -1,8 +1,12 @@
 package com.patrickgrimard.examples;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.script.ScriptTemplateConfigurer;
@@ -31,5 +35,24 @@ public class Application extends WebMvcConfigurerAdapter {
         configurer.setRenderFunction("render");
         configurer.setSharedEngine(false);
         return configurer;
+    }
+
+    @Bean
+    public CommandLineRunner init(final ItemRepository itemRepository) {
+        return (args) -> {
+            itemRepository.save(new Item("JavaScript"));
+            itemRepository.save(new Item("React"));
+            itemRepository.save(new Item("React Router"));
+            itemRepository.save(new Item("Redux"));
+            itemRepository.save(new Item("RxJS"));
+        };
+    }
+
+    @Configuration
+    public static class RestConfiguration extends RepositoryRestConfigurerAdapter {
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+            config.exposeIdsFor(Item.class).setBasePath("/api");
+        }
     }
 }
