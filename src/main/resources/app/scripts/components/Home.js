998caacfd527a06023615eebdbfac309372024d4
Patrick Grimard <pgrimard@gmail.com>
2016-11-25 16:06:57 -0500

    Home component now has an ItemList component embedded in it.

--- src/main/resources/app/scripts/components/Home.js
+++ src/main/resources/app/scripts/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ItemList from 'components/item/ItemList';
 
 export default function Home() {
   return (
     <div>
-      <h2>I am the root component.</h2>
+      <h2>Here, have some items!</h2>
+      <ItemList/>
     </div>
   );
 }
