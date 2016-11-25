d9aa639f549adecedf12f79fcd87f402654158d8
Patrick Grimard <pgrimard@gmail.com>
2016-11-25 16:07:17 -0500

    client and server side scripts now render a redux store.

--- src/main/resources/app/scripts/client.js
+++ src/main/resources/app/scripts/client.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {Provider} from 'react-redux';
+import reducer from './reducers';
 import App from 'components/App';
 
+const store = createStore(reducer, window.__PRELOADED_STATE__, applyMiddleware(thunkMiddleware));
+
 const markup = (
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
 );
 
 render(markup, document.getElementById('app'));
--- src/main/resources/app/scripts/server.js
+++ src/main/resources/app/scripts/server.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
 import {ServerRouter, createServerRenderContext} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import {Provider} from 'react-redux';
+import reducer from './reducers';
 import App from 'components/App';
 
 window.render = (template, model) => {
   const context = createServerRenderContext();
-  const state = JSON.parse(model.get('state'));
+  const req = JSON.parse(model.get('req'));
+  const initialState = JSON.parse(model.get('initialState'));
+
+  const store = createStore(reducer, initialState, applyMiddleware(thunkMiddleware));
+
   const markup = renderToString(
-    <ServerRouter location={state.location} context={context}>
-      <App/>
-    </ServerRouter>
+    <Provider store={store}>
+      <ServerRouter location={req.location} context={context}>
+        <App/>
+      </ServerRouter>
+    </Provider>
   );
-  return template.replace('{{output}}', markup);
+
+  return template
+    .replace('SERVER_RENDERED_HTML', markup)
+    .replace('SERVER_RENDERED_STATE', JSON.stringify(initialState));
 };
