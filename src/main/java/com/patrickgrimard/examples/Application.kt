a958ae892b1bcc6fb95df8ae9d3b5407c6dfdd7f
Patrick Grimard <pgrimard@gmail.com>
2016-12-03 17:16:10 -0500

    Application converted to Kotlin

---
+++ src/main/java/com/patrickgrimard/examples/Application.kt
@@ -0,0 +1,58 @@
+package com.patrickgrimard.examples
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+import org.springframework.web.servlet.ViewResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.view.script.ScriptTemplateConfigurer
+import org.springframework.web.servlet.view.script.ScriptTemplateViewResolver
+
+@SpringBootApplication
+open class Application : WebMvcConfigurerAdapter() {
+
+    @Bean
+    open fun viewResolver(): ViewResolver {
+        return ScriptTemplateViewResolver("/public/", ".html")
+    }
+
+    @Bean
+    open fun scriptTemplateConfigurer(): ScriptTemplateConfigurer {
+        val configurer = ScriptTemplateConfigurer()
+        configurer.engineName = "nashorn"
+        configurer.setScripts(
+                "static/polyfill.js",
+                "public/server.js"
+        )
+        configurer.renderFunction = "render"
+        configurer.isSharedEngine = false
+        return configurer
+    }
+
+    @Bean
+    open fun init(itemRepository: ItemRepository) = CommandLineRunner {
+        itemRepository.save(Item("JavaScript", 0))
+        itemRepository.save(Item("React", 1))
+        itemRepository.save(Item("React Router", 2))
+        itemRepository.save(Item("Redux", 0))
+        itemRepository.save(Item("RxJS", 4))
+    }
+
+    @Configuration
+    open class RestConfiguration : RepositoryRestConfigurerAdapter() {
+        override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration?) {
+            config!!.exposeIdsFor(Item::class.java).setBasePath("/api")
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
