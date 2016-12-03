ff598bc8aa6c9b9a388b25fdd36226070186925b
Patrick Grimard <pgrimard@gmail.com>
2016-12-03 17:06:15 -0500

    IndexController converted to Kotlin

---
+++ src/main/java/com/patrickgrimard/examples/IndexController.kt
@@ -0,0 +1,37 @@
+package com.patrickgrimard.examples
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Created on 2016-11-22
+
+ * @author Patrick
+ */
+@Controller
+class IndexController(private val itemRepository: ItemRepository) {
+
+    @GetMapping("/{path:(?!.*.js|.*.css|.*.jpg|api).*$}")
+    @Throws(JsonProcessingException::class)
+    fun index(model: Model, request: HttpServletRequest): String {
+        val mapper = ObjectMapper()
+
+        val req = HashMap<String, Any>()
+        val root = if (request.servletPath == "/index.html") "/" else request.servletPath
+        if (request.queryString != null)
+            req.put("location", String.format("%s?%s", root, request.queryString))
+        else
+            req.put("location", root)
+        model.addAttribute("req", mapper.writeValueAsString(req))
+
+        val initialState = HashMap<String, Any>()
+        initialState.put("items", itemRepository.findAll())
+        model.addAttribute("initialState", mapper.writeValueAsString(initialState))
+        return "index"
+    }
+}
