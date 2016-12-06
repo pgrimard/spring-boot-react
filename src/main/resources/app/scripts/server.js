233853ed693bb9184b76e62502859e958cb2a454
Patrick Grimard <pgrimard@gmail.com>
2016-12-06 17:44:21 -0500

    Server side JS render function now accepts a 3rd parameter containing a message resource bundle as a JSON object.

--- src/main/resources/app/scripts/server.js
+++ src/main/resources/app/scripts/server.js
@@ -8,7 +8,7 @@ import serialize from 'serialize-javascript';
 import reducer from './reducers';
 import App from 'components/App';
 
-window.render = (template, model) => {
+window.render = (template, model, messages) => {
   const context = createServerRenderContext();
   const {req, initialState} = model;
   const store = createStore(reducer, JSON.parse(initialState), applyMiddleware(thunkMiddleware));
@@ -23,5 +23,6 @@ window.render = (template, model) => {
 
   return template
     .replace('SERVER_RENDERED_HTML', markup)
-    .replace('SERVER_RENDERED_STATE', serialize(JSON.parse(initialState), {isJSON: true}));
+    .replace('SERVER_RENDERED_STATE', serialize(JSON.parse(initialState), {isJSON: true}))
+    .replace('SERVER_RENDERED_MESSAGES', serialize(messages, {isJSON: true}))
 };
